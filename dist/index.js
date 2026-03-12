e77add8e54ac361adb00e8703eb58bd5cbac24f5
Ozan Can CUYAR <ozncncyr@outlook.com>
2026-03-13 00:08:23 +0300

    feat: simplify index.ts by removing unused code and updating myName variable

--- dist/index.js
+++ dist/index.js
@@ -1,15 +1,4 @@
 "use strict";
-const age = 18;
-[...Array(100).fill(null)].forEach(() => {
-    console.log("Hello, TypeScript!");
-});
-const unusedVariable = 42;
-function unusedFunction() {
-    return 'Bu fonksiyon kullanılmıyor';
-}
-function greet(name) {
-    return `Hello, ${name}!`;
-}
-;
-const date = new Date();
+let myName = "Ozan";
+myName = "Ozan Can";
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,3 @@
-const age = 18;
+let myName = "Ozan";
 
-[...Array(100).fill(null)].forEach(() => {
-    console.log("Hello, TypeScript!");
-});
-const unusedVariable = 42;
-
-function unusedFunction() {
-  return 'Bu fonksiyon kullanılmıyor';
-}
-function greet(name: string): string {
-    return `Hello, ${name}!`;
-};
-
-const date = new Date();
+myName = "Ozan Can";
