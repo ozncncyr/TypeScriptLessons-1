4cfbcaab045f08fca525ad62f928c9658791d63d
Ozan Can CUYAR <ozncncyr@outlook.com>
2026-03-13 00:03:31 +0300

    Add TypeScript package configuration (v5.9.3) to project

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,15 @@
 "use strict";
-console.log("Hello, TypeScript!");
+const age = 18;
+[...Array(100).fill(null)].forEach(() => {
+    console.log("Hello, TypeScript!");
+});
+const unusedVariable = 42;
+function unusedFunction() {
+    return 'Bu fonksiyon kullanılmıyor';
+}
+function greet(name) {
+    return `Hello, ${name}!`;
+}
+;
+const date = new Date();
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("Hello, TypeScript!");
+const age = 18;
+
+[...Array(100).fill(null)].forEach(() => {
+    console.log("Hello, TypeScript!");
+});
+const unusedVariable = 42;
+
+function unusedFunction() {
+  return 'Bu fonksiyon kullanılmıyor';
+}
+function greet(name: string): string {
+    return `Hello, ${name}!`;
+};
+
+const date = new Date();
