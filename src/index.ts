4159fbb7bdbcff4ff08636e235cd6aeca867224c
Ozan Can CUYAR <ozncncyr@outlook.com>
2026-03-14 15:26:10 +0300

    feat: refactor index.ts by commenting out unused code and adding LogPerson function

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
-const myArray: [string, string, number] = ["Ozan", "Cuyar", 29];
+// const myArray: [string, string, number] = ["Ozan", "Cuyar", 29];
 
-function f():[string, number] {
-    return [`${myArray[0]} ${myArray[1]}`, myArray[2]];
+// function f():[string, number] {
+//     return [`${myArray[0]} ${myArray[1]}`, myArray[2]];
+// }
+
+// const myInfo = f();
+
+function LogPerson(name: string, surname: string, age: number) {
+    console.log(`I am ${name} ${surname}. I am ${age} years old.`)
 }
 
-const myInfo = f();
-
-console.log(myInfo);
+LogPerson(...["Ozan", "Cuyar", 29]);
