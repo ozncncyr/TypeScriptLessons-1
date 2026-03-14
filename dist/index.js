3b23a89debcd36c9358d583751901c0df5635ab5
Ozan Can CUYAR <ozncncyr@outlook.com>
2026-03-14 15:52:45 +0300

    feat: add repeatName function to log name multiple times and comment out previous code

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,26 @@
 "use strict";
-const myArray = ["Ozan", "Cuyar", 29];
-function f() {
-    return [`${myArray[0]} ${myArray[1]}`, myArray[2]];
-}
-const myInfo = f();
-console.log(myInfo);
+// const myArray: [string, string, number] = ["Ozan", "Cuyar", 29];
+// function f():[string, number] {
+//     return [`${myArray[0]} ${myArray[1]}`, myArray[2]];
+// }
+// const myInfo = f();
+// const LogPerson = async (name: string, surname: string, age: number) => {
+//     console.log(`I am ${name} ${surname}. I am ${age} years old.`)
+// }
+// LogPerson("Ozan", "Cuyar", 29);
+// const LogPerson = (params: { name: string; surname: string; age: number }): Promise<string> => {
+//     const { name, surname, age } = params;
+//     const msg = `I am ${name} ${surname}. I am ${age} years old.`;
+//     return Promise.resolve(msg);
+// }
+// LogPerson({ name: "Ozan", surname: "Cuyar", age: 29 }).then((msg) => {
+//     console.log(msg);
+// })
+// HOMEWORK: A FUNCTION THAT TYPING THE NAME AS MANY TIMES AS THE NUMBER IN THE SECOND PARAMETER
+const repeatName = (name, times) => {
+    for (let i = 0; i < times; i++) {
+        console.log((i + 1) + ". " + name);
+    }
+};
+repeatName("Ozan", 5);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,29 @@
 
 // const myInfo = f();
 
-function LogPerson(name: string, surname: string, age: number) {
-    console.log(`I am ${name} ${surname}. I am ${age} years old.`)
+// const LogPerson = async (name: string, surname: string, age: number) => {
+//     console.log(`I am ${name} ${surname}. I am ${age} years old.`)
+// }
+
+// LogPerson("Ozan", "Cuyar", 29);
+
+// const LogPerson = (params: { name: string; surname: string; age: number }): Promise<string> => {
+//     const { name, surname, age } = params;
+//     const msg = `I am ${name} ${surname}. I am ${age} years old.`;
+//     return Promise.resolve(msg);
+// }
+
+// LogPerson({ name: "Ozan", surname: "Cuyar", age: 29 }).then((msg) => {
+//     console.log(msg);
+// })
+
+
+// HOMEWORK: A FUNCTION THAT TYPING THE NAME AS MANY TIMES AS THE NUMBER IN THE SECOND PARAMETER
+
+const repeatName = (name: string, times: number) => {
+    for (let i = 0; i < times; i++) {
+        console.log((i + 1) + ". " + name);
+    }
 }
 
-LogPerson(...["Ozan", "Cuyar", 29]);
+repeatName("Ozan", 5);
