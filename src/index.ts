be0b4ab123f30602025823c0a0f093b33e68684b
Ozan Can CUYAR <ozncncyr@outlook.com>
2026-03-25 23:05:10 +0300

    feat: add MongoResponse and BooksResponse interfaces with getBookResponse function

--- src/index.ts
+++ src/index.ts
@@ -45,12 +45,47 @@
 
 //////////////////
 
-interface IFunction {
-    (name: string, repeat: number): void;
+// interface IFunction1 {
+//     (name: string, repeat: number): void;
+// }
+
+// const myFunction: IFunction1 = (name, repeat) => {
+//     console.log("\n" + name.repeat(repeat));
+// }
+
+// interface IFunction2 extends IFunction1 {
+//     (age: number, repeat: number): void;
+// }
+
+// const myFunction2: IFunction2 = (age, repeat) => {
+//     console.log("\n" + age.toString().repeat(repeat));
+// }
+
+// myFunction("Hello", 3); // Output: HelloHelloHello
+// myFunction2(5, 3); // Output: 555
+
+///////////////////
+
+interface MongoResponse {
+    _id: string;
+    createdAt: Date;
+    updatedAt: Date;
+}
+ 
+interface BooksResponse extends MongoResponse {
+    title: string;
+    author: string;
+    publishedYear: number;
 }
 
-const myFunction: IFunction = (name, repeat) => {
-    console.log("\n" + name.repeat(repeat));
+function getBookResponse(): BooksResponse {
+    return {
+        _id: "1234567890",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        title: "The Great Gatsby",
+        author: "F. Scott Fitzgerald",
+        publishedYear: 1925
+    };
 }
 
-myFunction("Hello", 3); // Output: HelloHelloHello
